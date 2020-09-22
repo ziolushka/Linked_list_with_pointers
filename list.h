4ea7e1cae2f1f0d7e2bc5d507e5ad1ec24ea874a
ziolushka <keepcalmandloved1@gmail.com>
2020-09-22 19:07:50 +0300

    Write linked list

---
+++ list.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Prints list*/
+void StringListPrint(char** list);
+/* Destroy list and set pointer to NULL. */
+void StringListDestroy(char*** list);
+/* Inserts value at the end of the list. */
+void StringListAdd(char*** list, char* str);
+/* Removes all occurrences of str in the list. */
+void StringListRemove(char*** list, char* str);
+/* Returns the number of items in the list. */
+int StringListSize(char** list);
+/* Returns the index position of the first occurrence of str in the list. */
+int StringListIndexOf(char*** list, char* str);
+/* Removes duplicate entries from the list. */
+void StringListRemoveDuplicates(char*** list);
+/* Replaces every occurrence of the before, in each of the string lists's strings, with after. */
+void StringListReplaceInStrings(char*** list, char* before, char* after);
+/* Sorts the list of strings in ascending order */
+void StringListSort(char*** list);
+
+#endif // LIST_H
---
+++ main.c
@@ -0,0 +1,45 @@
+#include <list.h>
+
+#define data 0
+#define next 1
+
+int main()
+{
+    char** head = NULL;
+
+    StringListAdd(&head, "111");
+    StringListAdd(&head, "555");
+    StringListAdd(&head, "333");
+    StringListAdd(&head, "222");
+    StringListAdd(&head, "555");
+    StringListAdd(&head, "444");
+    StringListAdd(&head, "444");
+    StringListPrint(head);
+
+    printf("Remove element %s\n","222");
+    StringListRemove(&head, "222");
+    StringListPrint(head);
+
+    printf("List size: %d\n",StringListSize(head));
+
+    printf("Index of \"%s\" element is %d \n","333", StringListIndexOf(&head, "333"));
+
+    printf("Replace strings \"%s\" with \"%s\"\n","444", "aaa");
+    StringListReplaceInStrings(&head,"444", "aaa");
+    StringListPrint(head);
+
+    printf("Remove duplicates \n");
+    StringListRemoveDuplicates(&head);
+    StringListPrint(head);
+
+    printf("Sort list \n");
+    StringListSort(&head);
+    StringListPrint(head);
+
+    printf("Destroy list \n");
+    StringListDestroy(&head);
+    StringListPrint(head);
+
+
+    return 0;
+}
